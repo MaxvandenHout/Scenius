1b1efaa9ff85f0defaebb6fb88d87da4378319da
mlvdhout <mlvdhout@gmail.com>
2020-09-08 22:04:40 +0200

    First version with working queue.

---
+++ OpdrachtScenius/OpdrachtScenius/Controllers/MessageController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OpdrachtScenius.Queue;
+
+namespace OpdrachtScenius.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class MessageController : ControllerBase
+    {
+        private readonly IQueueHandler queueHandler;
+        public MessageController(IQueueHandler queueHandler)
+        {
+            this.queueHandler = queueHandler;
+        } 
+
+        // Post api/message
+        [HttpPost("message")]
+        public void SendMessage(Models.Message message)
+        {
+            System.Console.WriteLine(message.Text);
+            this.queueHandler.QueueMessage(message);
+        }    
+
+        // Get api/message
+        //[HttpGet]
+        //public ActionResult<IEnumerable<Models.Message>> GetMessagesHistory()
+        //{
+        //    // DB call return all
+        //}
+
+     
+    }
+}
